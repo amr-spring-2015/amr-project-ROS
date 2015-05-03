6bb1fcb99e44d53cea3fb9db645b7eefdf49c457
amr-spring-2015 <jakedostal@gmail.com>
2015-05-03 06:29:59 -0600

    Delete test.m

--- src/amrprojpkg/scripts/test.m
+++
@@ -1,6 +0,0 @@
-fb='2'
-while fb ~='Y'
-        %Subscribing to 'feedback' topic created by Pithon script
-        disp('stuck in first feedback loop') %diagnostic
-       fb= input('Enter a charaacter','s')
-end
